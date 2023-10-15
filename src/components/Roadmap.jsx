dfc45185d9f48c4418e31aebdec34f7b18a12f33
sheriff ibrahim <ibrahimsheriff999@gmail.com>
2023-10-15 14:44:36 +0100

    Redesigned roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { DataContext } from "../context/DataContext";
-import {FaCheck} from 'react-icons/fa'
+import { FaCheck } from "react-icons/fa";
 
 export default function Roadmap() {
    const {
@@ -9,29 +9,133 @@ export default function Roadmap() {
    return (
       <div className="roadmap bg-tblack py-5 p-5 text-light" id="roadmap">
          <div className="container">
-            <div className="header py-5">
-               <small className="bg-purple badge text-light">Our Detailed Plan</small> <br />
+            <div className="header py-5 text-center">
+               <small className="bg-purple badge text-light">
+                  Our Detailed Plan
+               </small>{" "}
+               <br />
                <span className="fw-bold fs-1">TTF Roadmap</span>
             </div>
          </div>
          <div className="container">
-            <div className="row">
-               {roadmap.map((rCard, index) => (
-                  <div className="col-lg-3 col-md-6 col-12 mb-4 " key={index}>
-                     <div className="r-card bg-dark-blur p-4 h-100">
-                        <div className="head d-flex mb-2 justify-content-center align-items-cent ">
-                         <span className="fw-bold fs-6">{index+1}</span>
-                        </div>
-                        <div className="body py-5">
-                           <ul className="m-0 p-0">
-                              {rCard.list.map(i => (
-                                 <li className="d-flex align-items-center gap-3 mb-2" key={i}> <small className="icon text-purple px-2 rounded bg-tblack"><FaCheck /> </small>{i}</li>
-                              ))}
-                           </ul>
-                        </div>
+            <div className="wrap">
+               {roadmap.map((road, index) => (
+                  <div className="row" key={index}>
+                     <div className="liner-left col-sm-6 ">
+                        {index % 2 !== 0 && (
+                           <div className="box">
+                              <div className="head-wrap d-flex justify-content-end">
+                                 <div className="head">
+                                    <span className="fw-bold text-purple fs-4">
+                                       Q{index + 1}
+                                    </span>
+                                 </div>
+                              </div>
+                              <div className="body-wrap d-flex justify-content-end ">
+                                 <div className="body">
+                                    <ul className="m-0 p-0">
+                                       {road.list.map((map, index) => (
+                                          <li
+                                             className="d-flex align-items-center gap-3 mb-2 justify-content-end"
+                                             key={index}
+                                          >
+                                             {" "}
+                                             {map}
+                                             <small className="icon text-purple px-2 rounded bg-tblack">
+                                                <FaCheck />{" "}
+                                             </small>
+                                          </li>
+                                       ))}
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                        )}
+                     </div>
+                     <div className="liner-right col-sm-6">
+                        {index % 2 === 0 && (
+                           <div className="box justify-content-end">
+                              <div className="head-wrap">
+                                 <div className="head">
+                                    <span className="fw-bold text-purple fs-4">
+                                       Q{index + 1}
+                                    </span>
+                                 </div>
+                              </div>
+                              <div className="body-wrap d-flex">
+                                 <div className="body">
+                                    <ul className="m-0 p-0">
+                                       {road.list.map((map, index) => (
+                                          <li
+                                             className="d-flex align-items-center gap-3 mb-2"
+                                             key={map}
+                                          >
+                                             {" "}
+                                             <small className="icon text-purple px-2 rounded bg-tblack">
+                                                <FaCheck />{" "}
+                                             </small>
+                                             {map}
+                                          </li>
+                                          // <li key={index}>{map}</li>
+                                       ))}
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                        )}
                      </div>
                   </div>
                ))}
+               {/* <div className="row">
+                  <div className="liner-left col-lg-6">
+                     <div className="box">
+                        <div className="head-wrap d-flex justify-content-end">
+                           <div className="head">
+                              <span className="fw-bold text-purple fs-4">
+                                 Q1
+                              </span>
+                           </div>
+                        </div>
+                        <div className="body-wrap d-flex justify-content-end">
+                           <div className="body">
+                              <ul className="m-0 p-0">
+                                 <li>Roadmap here</li>
+                                 <li>Roadmap here</li>
+                                 <li>
+                                    Roadmap RoadmapRoadmapRoadmapRoadmap here
+                                 </li>
+                                 <li>Roadmap here</li>
+                                 <li>Roadmap here</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div className="liner-right col-lg-6">
+                     <div className="box justify-content-end">
+                        <div className="head-wrap">
+                           <div className="head">
+                              <span className="fw-bold text-purple fs-4">
+                                 Q1
+                              </span>
+                           </div>
+                        </div>
+                        <div className="body-wrap d-flex">
+                           <div className="body">
+                              <ul className="m-0 p-0">
+                                 <li>Roadmap here</li>
+                                 <li>Roadmap here</li>
+                                 <li>
+                                    Roadmap RoadmapRoadmapRoadmapRoadmap here
+                                 </li>
+                                 <li>Roadmap here</li>
+                                 <li>Roadmap here</li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div> */}
             </div>
          </div>
       </div>
